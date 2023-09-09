adc9a5586237fc85f2ea180796016ffec351b4bf
Brako <gabobrako@gmail.com>
2023-09-09 13:40:54 -0400

    fixing tabulation in every document

--- girdle-sizin-app/src/components/MtKg.jsx
+++ girdle-sizin-app/src/components/MtKg.jsx
@@ -1,40 +1,43 @@
 export const MtKg = () => {
   return (
     <>
-        <label htmlFor="altura">Altura (Metros y centimetros):</label>
-        <br />
-        <select id="metro" name="metro" required>
-            <option value="" disabled defaultValue="">Metro</option>
-            <option value="1">1 M</option>
-            <option value="2">2 M</option>
-        
-        </select>
-        <input 
-            id="centimetro" 
-            name="centimetro" 
-            type="number"
-            placeholder="00" 
-            maxLength="2" 
-            required 
-            onInput={(e) => {
-                e.target.value = e.target.value.replace(/[^\d]/, '').slice(0, 2);
-              }}/>
-        <br />
+      <label htmlFor="altura">Altura (Metros y centimetros):</label>
+      <br />
+      <select id="metro" name="metro" required>
+        <option value="" disabled defaultValue="">
+          Metro
+        </option>
+        <option value="1">1 M</option>
+        <option value="2">2 M</option>
+      </select>
+      <input
+        id="centimetro"
+        name="centimetro"
+        type="number"
+        placeholder="00"
+        maxLength="2"
+        required
+        onInput={(e) => {
+          e.target.value = e.target.value.replace(/[^\d]/, "").slice(0, 2);
+        }}
+      />
+      <br />
 
-        <label htmlFor="peso">Peso (Kg):</label>
-        <br />
-        <input 
-            id="peso" 
-            name="peso" 
-            type="number" 
-            placeholder="000" 
-            minLength="2" 
-            maxLength="3" 
-            required 
-            onInput={(e) => {
-                e.target.value = e.target.value.replace(/[^\d]/, '').slice(0, 3);
-              }}/>
-        <br />
+      <label htmlFor="peso">Peso (Kg):</label>
+      <br />
+      <input
+        id="peso"
+        name="peso"
+        type="number"
+        placeholder="000"
+        minLength="2"
+        maxLength="3"
+        required
+        onInput={(e) => {
+          e.target.value = e.target.value.replace(/[^\d]/, "").slice(0, 3);
+        }}
+      />
+      <br />
     </>
-  )
-}
+  );
+};
